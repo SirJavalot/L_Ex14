874520f7a85a08d193dc309c14c07ce6620b7a9b
SirJavalot <dennis155@hotmail.com>
2015-10-24 13:44:24 +0100

    first commit

---
+++ L_EX_14/src/Ex1.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class Ex1 extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("test");
+
+	}
+
+}

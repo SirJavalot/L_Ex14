996cdc912938a12ed2fa96904fa9cb8719a0a058
SirJavalot <dennis155@hotmail.com>
2015-10-24 14:12:35 +0100

    Second

--- L_EX_14/src/ClockPane.java
+++ L_EX_14/src/ClockPane.java
@@ -1,4 +1,9 @@
 import javafx.scene.layout.Pane;
+import java.util.GregorianCalendar;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
 
 
 public class ClockPane extends Pane {

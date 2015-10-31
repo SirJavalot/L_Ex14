f95488ed06f60449d84b54a7e2291eec9c057c1a
SirJavalot <dennis155@hotmail.com>
2015-10-31 17:00:01 +0000

    2

--- L_EX_14/src/EX1410.java
+++ L_EX_14/src/EX1410.java
@@ -1,6 +1,8 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
@@ -13,7 +15,11 @@ public class EX1410 extends Application {
 		Line line1 = new Line(100,50,100,400);
 		
 		Line line2 = new Line(300,50,300,400);
-		pane.getChildren().addAll(line1,line2);
+		
+		Ellipse e = new Ellipse(200, 50, 100, 30);
+		e.setFill(Color.WHITE);
+		e.setStroke(Color.BLACK);
+		pane.getChildren().addAll(line1,line2,e);
 		Scene scene = new Scene(pane,400,450);
 		primaryStage.setTitle("14.10");
 		primaryStage.setScene(scene);

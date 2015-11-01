d31a62c7a0bf509cb9d51dda6772b9e227e26f8d
SirJavalot <dennis155@hotmail.com>
2015-11-01 11:10:25 +0000

    solution

--- L_EX_14/src/EX1410.java
+++ L_EX_14/src/EX1410.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
 import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Line;
 import javafx.stage.Stage;
@@ -17,9 +18,16 @@ public class EX1410 extends Application {
 		Line line2 = new Line(300,50,300,400);
 		
 		Ellipse e = new Ellipse(200, 50, 100, 30);
+		Arc arc = new Arc(200, 400, 100, 30, 0, -180);
+		Arc arc2 = new Arc(200, 400, 100, 30, 0, 180);
+		arc2.getStrokeDashArray().addAll(6.0,21.0);
+		arc2.setFill(Color.TRANSPARENT);
+		arc2.setStroke(Color.BLACK);
+		arc.setFill(Color.TRANSPARENT);
+		arc.setStroke(Color.BLACK);
 		e.setFill(Color.WHITE);
 		e.setStroke(Color.BLACK);
-		pane.getChildren().addAll(line1,line2,e);
+		pane.getChildren().addAll(line1,line2,e,arc,arc2);
 		Scene scene = new Scene(pane,400,450);
 		primaryStage.setTitle("14.10");
 		primaryStage.setScene(scene);
